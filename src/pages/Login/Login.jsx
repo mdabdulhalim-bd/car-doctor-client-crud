57db67f6c7e07d2af0098cefe0fd76ac25416e26
mdabdulhalim <htmlcssjs2023@gmail.com>
2024-01-09 10:21:20 +0600

    jwt token

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
@@ -6,17 +6,42 @@ import { AuthContext } from '../../providers/AuthProvider';
 const Login = () => {
 
     const { signIn } = useContext(AuthContext);
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    const navigate = useNavigate();
 
     const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, email, password)
+        
+        
         signIn(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user);
+                const loggedUser = {
+                    email: user.email
+                }
+                // console.log(user);
+                
+
+                fetch('http://localhost:5000/jwt', {
+                    method:'POST',
+                    headers:{
+                        'content-type': 'application/json'
+                    },
+                    body:JSON.stringify(loggedUser)
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log('jwt response',data)
+
+                    // warning: local storage is not best ( it's second best)
+                    // Save token to the localstorage
+                    localStorage.setItem('car-access-token', data.token)
+                    navigate(from, {replace:true})
+                })
             })
             .catch(error => console.log(error));
     }
